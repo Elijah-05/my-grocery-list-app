8137d3cb5c8fb4f079df4867b8e467b5a8445a12
Elijah-05 <125945721+Elijah-05@users.noreply.github.com>
2023-05-04 23:15:28 +0300

    Initial Deploy

--- src/Component/Summary/Summary.js
+++ src/Component/Summary/Summary.js
@@ -3,12 +3,12 @@ import './summary.css'
 
 function ItemSummary({items}) {
   let totalPrice;
-  totalPrice = items.map((item) => item.price * item.quantity).reduce((acc, cur) => acc + cur ,0)
-  
+  totalPrice = Array.isArray(items) && items.length > 0 ? items.map((item) => item.price * item.quantity).reduce((acc, cur) => acc + cur ,0) : 0
+  let itemLength = Array.isArray(items) ? items.length : 0
   return (
     <footer>
         <div className='footer-container'>
-            <p className='number-of-items'>{items.length} Items</p>
+            <p className='number-of-items'>{itemLength} Items</p>
             <div className='total-container'>
                 <h3>Total</h3>
                 <p><span className='total-price'>{totalPrice}</span> Birr</p>
